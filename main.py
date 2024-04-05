9452490e073ad137c1db2d5be7655c24e8db6808
Eduardo <eduardo.sbassani@gmail.com>
2024-04-05 15:08:16 +0300

    Using YOLOv8 from ultralytics with dataset from roboflow

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-from losses import yolo_loss
-from models import create_yolo_model
+# Load a model
+from ultralytics import YOLO
 
+model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
-def compile_model():
-    shape = (448, 448, 3)
-    num_classes = 20
-
-    model = create_yolo_model(shape, num_classes)
-
-    model.compile(optimizer='adam', loss=yolo_loss)
-
-
-if __name__ == '__main__':
-    compile_model()
+# Use the model
+model.train(data="resources/data.yaml", epochs=2)  # train the model
+metrics = model.val()  # evaluate model performance on the validation set
